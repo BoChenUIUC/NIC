d664ffb60bea68dfc2b6c50c85aa24b6e994e924
Bo Chen <bochen1993cs@gmai.com>
2023-05-07 23:14:20 -0500

    add mlp codec

--- models.py
+++ models.py
@@ -325,7 +325,9 @@ class MLPCodec(CompressionModel):
         y_hat, y_likelihoods = self.entropy_bottleneck(y)
 
         x_coord = torch.arange(H, device = x.device, dtype = torch.long)
-        x_emb = self.x_mlp(x_coord).repeat(B,1,1,W)
+        x_emb = self.x_mlp(x_coord)
+        print(x_emb.size())
+        x_emb = x_emb.repeat(B,1,1,W)
         print(x_emb.size())
 
         x_emb = self.x_mlp(x_coord).permute(0,3,1,2)
